92a33e14e72985f21ae4f48113410af28e2eddc9
chris martinez and mark lawson <chris@>
2016-08-17 13:09:15 -0700

    create the form and add javascript to translate words beginning with q

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+/////////////back end/////////////
+var vowels = ["a", "e", "i", "o", "u", "y"];
+var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"];
+var allLetters = vowels.concat(consonants);
+var firstLetter;
+
+var translate = function(word) {
+  if (word.charAt(0) === "q") {
+    firstLetter = "qu";
+    word = word.slice(2, word.length);
+    return word + firstLetter + "ay";
+  }
+  else if ((consonants.indexOf(word.charAt(0)) >= 0) || (word.charAt(0) === "y")) {
+
+
+    // for (var i = 1)
+  }
+  else {
+    return word;
+  }
+}
+
+
+/////////////front end/////////////
+
+$(function(){
+  $("#textInputButton").click(function(event){
+    event.preventDefault();
+    var textInput = $('#textInput').val().toLowerCase();
+    var textArray = textInput.split(" ");
+
+    for (var i = 0; i<textArray.length; i++) {
+      textArray[i] = translate(textArray[i]);
+    }
+
+
+    var results = textArray.join(" ");
+    $('#results').text(results);
+  })
+
+})
